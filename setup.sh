b7144f5b4af2e0834ab604d0316ac1065ce30023
Ashish Kumar Joy <34642693+ashishkujoy@users.noreply.github.com>
2022-10-19 19:38:07 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -nv true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
