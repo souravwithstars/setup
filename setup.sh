a21912a505525259258f2f0fc5e05c8d8acf32de
Ashish Kumar Joy <34642693+ashishkujoy@users.noreply.github.com>
2022-10-19 19:39:18 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,3 +12,5 @@ if test ! $(which brew); then
   echo "Installing homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+echo "setup completed..."
